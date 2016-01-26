b05fc939faa6397d8b500fd279a34e16bf7b8bf8
John Gaunt <redfive@jgaunt.com>
2016-01-26 15:48:07 -0800

    bash completion setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,34 +1,50 @@
 #!/usr/bin/env bash
 
-# get the local directory of the script, assume that the bash stuff is in pwd/../bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#echo $DIR
+function error_exit {
+  echo "$1" 1>&2
+  exit 1
+}
 
-BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-#echo $BASE
+# base location of the dotfiles repo
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-# source the bash completion scripts
 # create the bash completion directory if it doesn't exist
-BASH_COMP_DIR=/home/jgaunt/.bash_completion
-if [[ ! -d ${BASH_COMP_DIR} ]]; then
-  mkdir ${BASH_COMP_DIR}
-  #TODO: error check here
+DEST_DIR=${HOME}/.bash_completion
+if [[ ! -d ${DEST_DIR} ]]; then
+  mkdir ${DEST_DIR} || error_exit "Failed to makedir ${DEST_DIR}"
 fi
 
-# loop over any source completion files to 
-BC="${BASE}/bash/completion"
-for f in $(find ${BC} -type f); do
-  # check to see if it matches the file in the BCD
-  # if it does, skip it
-  # if not and a file exists, remove it
-  # link the file
-  echo $f
+# loop over any source completion files, make sure they are installed and sourced
+SRC_DIR="${DOTFILES_DIR}/bash/completion"
+for f in $(find ${SRC_DIR} -type f); do
+
+  # capture the name of the file to use in destination operation
+  filename="$(basename $f)"
+
+  # Does the file exist in the completion directory
+  # TODO: check for link or directory here too
+  if [[ ! -f ${DEST_DIR}/${filename} ]]; then
+    #echo "File absent -- link me!"
+    $(ln -s ${f} ${DEST_DIR}/${filename}) || error_exit "Failed to link ${f} to ${DEST_DIR}/${filename}"
+  fi
+
+  # check to see if it matches the file in the BCD, if not, update it
+  # TODO: maybe use `readlink` here? 
+  if ! $(diff ${f} ${DEST_DIR}/${filename} >/dev/null 2>&1); then
+    #echo Files are different -- link me!
+    $(ln -s ${f} ${DEST_DIR}/${filename}) || error_exit "Failed to link ${f} to ${DEST_DIR}/${filename}"
+  fi
 done 
 
-if [[ -d ${BASH_COMP_DIR} ]]; then
-  for f in $(find ${BASH_COMP_DIR} -type f)
-  do
-    source $f
+# TODO - VIM configuration
+# TODO - pathogen
+# TODO - bundles like color schemes
+
+# source all the bash completion scripts in the user's completion dir
+if [[ -d ${DEST_DIR} ]]; then
+  for f in $(find -L ${DEST_DIR} -type f); do
+    echo "Sourcing ${f}"
+    source ${f} || error_exit "Failed trying to source ${f}"
   done 
 fi
 
