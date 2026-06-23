a0f192f7140f582bc8499573a6eecebfe896e0f5
John Gaunt <redfive@jgaunt.com>
2026-06-23 15:11:46 -0700

    add shortcuts for connecting to openshift hosts

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -13,3 +13,11 @@ alias cdj='cd $HOME/local/JENKINS'
 # Panora commands
 alias hrs='info-sb hostsroles $(hostname)'
 alias vl='vault login -method=ldap username=jgaunt'
+
+alias jmpsv7='hop pdsvlvjmpsrv01'
+alias jmpiad1='hop pduulvjmpsrv01'
+alias jmpiad2='hop pduulvjmpsrv02'
+
+# openshift work
+alias ocpiad='hop pduulpocpbld01'
+alias ocpsv7='hop pdsvlpocpbld01'
